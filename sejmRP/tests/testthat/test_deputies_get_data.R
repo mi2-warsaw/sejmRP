2374763da0f9db0420667847b36946f33061ff28
Piotr Smuda <smudap@student.mini.pw.edu.pl>
2015-09-23 11:36:15 +0200

    tests improvements

--- sejmRP/tests/testthat/test_deputies_get_data.R
+++ sejmRP/tests/testthat/test_deputies_get_data.R
@@ -1,12 +1,23 @@
 test_that("result of function", {
-  expect_equal(class(deputies_get_data()), "data.frame")
+  expect_equal(class(deputies_get_data("active")), "data.frame")
+})
+
+test_that("result of function", {
+  expect_equal(class(deputies_get_data("inactive")), "data.frame")
 })
 
 test_that("columns of table", {
-  expect_equal(ncol(deputies_get_data()), 2)
+  expect_equal(ncol(deputies_get_data("active")), 2)
 })
 
+test_that("columns of table", {
+  expect_equal(ncol(deputies_get_data("inactive")), 2)
+})
 
 test_that("rows of table", {
-  expect_more_than(nrow(deputies_get_data()), 0)
+  expect_more_than(nrow(deputies_get_data("active")), 0)
+})
+
+test_that("rows of table", {
+  expect_more_than(nrow(deputies_get_data("inactive")), 0)
 })
--- sejmRP/tests/testthat/test_vottes_match_deputies_ids.R
+++ sejmRP/tests/testthat/test_vottes_match_deputies_ids.R
@@ -1,18 +1,18 @@
 test_that("result of function", {
-  expect_equal(class(votes_match_deputies_ids("sejmrp", "sejmrp", "password", "192.168.137.38",
+  expect_equal(class(votes_match_deputies_ids("sejmrp", "reader", "qux94874", "192.168.137.38",
     "http://www.sejm.gov.pl/Sejm7.nsf/agent.xsp?symbol=klubglos&IdGlosowania=37494&KodKlubu=PO",
     .Platform$OS.type == "windows")),
     "data.frame")
 })
 
 test_that("columns of table", {
-  expect_equal(ncol(votes_match_deputies_ids("sejmrp", "sejmrp", "password", "192.168.137.38",
+  expect_equal(ncol(votes_match_deputies_ids("sejmrp", "reader", "qux94874", "192.168.137.38",
     "http://www.sejm.gov.pl/Sejm7.nsf/agent.xsp?symbol=klubglos&IdGlosowania=37494&KodKlubu=SLD",
     .Platform$OS.type == "windows")), 3)
 })
 
 test_that("rows of table", {
-  expect_more_than(nrow(votes_match_deputies_ids("sejmrp", "sejmrp", "password", "192.168.137.38",
+  expect_more_than(nrow(votes_match_deputies_ids("sejmrp", "reader", "qux94874", "192.168.137.38",
     "http://www.sejm.gov.pl/Sejm7.nsf/agent.xsp?symbol=klubglos&IdGlosowania=37494&KodKlubu=PiS",
     .Platform$OS.type == "windows")), 0)
 })
