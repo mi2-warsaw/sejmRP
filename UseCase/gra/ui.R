eb7c4df27ccf0c32b9ea9558cd09ffefd48877e5
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-02-13 01:48:28 +0100

    ?gra

---
+++ UseCase/gra/ui.R
@@ -0,0 +1,25 @@
+load("dane.rda")
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
